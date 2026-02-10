af7c6247e993c1cf98f6b98a254329404aa78ab3
BINDU TEJESWAR REDDY LINGALA <84988710+Tejeswareddy0005@users.noreply.github.com>
2026-02-10 17:26:09 +0530

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
-This line is called a shebang (#!).
-It is placed at the very top of a shell script to tell the system which interpreter should run the script.
+#This line is called a shebang (#!).
+#It is placed at the very top of a shell script to tell the system which interpreter should run the script.
+# Simple Calculator Script
+
+echo "Enter first number:"
+read num1
+
+echo "Enter second number:"
+read num4
+
+echo "Choose operation: +  -  *  /"
+read op
+
+case $op in
+  +)
+    result=$((num1 + num2))
+    echo "Result: $result"
+    ;;
+  -)
+    result=$((num1 - num2))
+    echo "Result: $result"
+    ;;
+  \*)
+    result=$((num1 * num2))
+    echo "Result: $result"
+    ;;
+  /)
+    if [ "$num2" -ne 0 ]; then
+      result=$((num1 / num2))
+      echo "Result: $result"
+    else
+      echo "Error: Division by zero!"
+    fi
+    ;;
+  *)
+    echo "Invalid operation"
+    ;;
+esac
